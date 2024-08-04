7547fdb3016d22a2a1ee794f81ae1b0aaa0e0214
Jimmy Wallace <jimmy.wallace145@gmail.com>
2024-08-04 00:15:19 -0400

    feat: Add CloudWatch client to emit custom metrics

--- appspec.py
+++ appspec.py
@@ -1,4 +1,25 @@
+"""
+AppSpec Generator
+
+This script is responsible for creating an `appspec.yml` file for
+CodeDeploy deployments to bump the WalterAIBackend lambda from its
+current version to the latest version. This script is run during
+builds executed by CodeBuild and is dumped to S3 as an artifact to
+be used by CodeDeploy to execute the deployment.
+"""
+
+import os
+
 import boto3
+from src.environment import get_domain
+
+DOMAIN = get_domain(os.getenv("DOMAIN"))
+
+REGION = os.getenv("AWS_REGION")
+
+LAMBDA_NAME = f"WalterAIBackend-{DOMAIN.value}"
+
+LAMBDA_ALIAS = f"WalterAIBackend-{DOMAIN.value}"
 
 APPSPEC_TEMPLATE = """
 version: 0.0
@@ -14,13 +35,20 @@ Resources:
 """
 
 with open("./appspec.yml", "w") as appspec:
-    lambda_client = boto3.client("lambda", region_name="us-east-1")
+    # create lambda client
+    lambda_client = boto3.client("lambda", region_name=REGION)
+
+    # get current version the lambda is pointing to
     current_version = lambda_client.get_alias(
-        FunctionName="WalterAIBackend-dev", Name="WalterAIBackend-dev"
+        FunctionName=LAMBDA_NAME, Name=LAMBDA_ALIAS
     )["FunctionVersion"]
-    target_version = lambda_client.list_versions_by_function(
-        FunctionName="WalterAIBackend-dev"
-    )["Versions"][-1]["Version"]
+
+    # get the latest version of the lambda
+    target_version = lambda_client.list_versions_by_function(FunctionName=LAMBDA_NAME)[
+        "Versions"
+    ][-1]["Version"]
+
+    # generate appspec file bumping lambda from current version to latest version
     appspec.write(
         APPSPEC_TEMPLATE.format(
             current_version=current_version, target_version=target_version
--- walter_ai.py
+++ walter_ai.py
@@ -1,7 +1,7 @@
 import json
-
-from src.clients import ddb, polygon, report_generator, bedrock
 from datetime import datetime, timedelta
+
+from src.clients import bedrock, cloudwatch, ddb, polygon, report_generator
 
 END_DATE = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
 START_DATE = END_DATE - timedelta(days=1)
@@ -9,15 +9,17 @@ START_DATE = END_DATE - timedelta(days=1)
 
 def lambda_handler(event, context) -> dict:
     # get stocks from ddb and get prices from polygon
-    stocks = []
-    for stock in ddb.get_stocks():
-        stocks.extend(polygon.get_prices(stock.symbol, START_DATE, END_DATE))
+    prices = []
+    stocks = ddb.get_stocks()
+    for stock in stocks:
+        prices.extend(polygon.get_prices(stock.symbol, START_DATE, END_DATE))
 
     # ingest stock data into report generator
-    report_generator.ingest_stocks(stocks)
+    report_generator.ingest_stocks(prices)
 
     # generate report for each user
-    for user in ddb.get_users():
+    users = ddb.get_users()
+    for user in users:
 
         # get stocks for user from ddb
         stocks = ddb.get_stocks_for_user(user)
@@ -30,5 +32,10 @@ def lambda_handler(event, context) -> dict:
 
         # TODO: print ai report until emailing via ses is implemented
         print(ai_report)
+
+    # emit metrics
+    cloudwatch.emit_metric_number_of_emails_sent(len(users))
+    cloudwatch.emit_metric_number_of_stocks_analyzed(len(stocks))
+    cloudwatch.emit_metric_number_of_subscribed_users(len(users))
 
     return {"statusCode": 200, "body": json.dumps("Walter AI")}
