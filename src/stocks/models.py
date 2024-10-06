21599e91d4e1edf8a3b3e8d5721e0a1ffa420970
Jimmy Wallace <jimmy.wallace145@gmail.com>
2024-10-06 09:18:21 -0400

    feat: Added stock portfolio unit tests

--- src/stocks/models.py
+++ src/stocks/models.py
@@ -8,19 +8,17 @@ from src.stocks.polygon.models import StockPrices
 @dataclass
 class Portfolio:
 
-    stocks: List[UserStock]
-    prices: Dict[str, StockPrices]  # stock symbol to stock prices
+    stocks: Dict[str, UserStock]
+    prices: Dict[str, StockPrices]
 
     def get_stocks(self) -> List[str]:
-        return [stock.stock_symbol for stock in self.stocks]
+        return self.stocks.keys()
 
     def get_latest_price(self, symbol: str) -> float:
         return self.prices[symbol].prices[-1].price
 
     def get_number_of_shares(self, symbol: str) -> float:
-        for stock in self.stocks:
-            if stock.stock_symbol == symbol:
-                return stock.quantity
+        return self.stocks[symbol].quantity
 
     def get_equity(self, symbol: str) -> float:
         price = self.get_latest_price(symbol)
@@ -29,6 +27,6 @@ class Portfolio:
 
     def get_total_equity(self) -> float:
         equity = 0
-        for stock in self.stocks:
-            equity += self.get_equity(stock.stock_symbol)
+        for stock in self.stocks.keys():
+            equity += self.get_equity(stock)
         return equity
