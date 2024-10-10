30448e16ee2f5d851a7ce2dedfbbf923a2004a69
Jimmy Wallace <jimmy.wallace145@gmail.com>
2024-10-09 21:08:52 -0400

    feat: Add newsletters queue and SQS trigger (#63)

---
+++ src/utils/events.py
@@ -0,0 +1,28 @@
+import json
+from dataclasses import dataclass
+
+from src.utils.log import Logger
+
+log = Logger(__name__).get_logger()
+
+
+@dataclass
+class Event:
+    email: str
+    dry_run: bool = True
+
+    def __str__(self) -> str:
+        return json.dumps(
+            {
+                "email": self.email,
+            },
+            indent=4,
+        )
+
+
+def parse_event(event: dict) -> Event:
+    records = event["Records"]
+    if len(records) != 1:
+        raise ValueError("More than a single message found in an SQS batch!")
+
+    return Event(email=json.loads(records[0]["body"])["email"])
