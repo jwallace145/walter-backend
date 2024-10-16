8d26837611865e4bb6b6133b495660db84e07aae
Jimmy Wallace <jimmy.wallace145@gmail.com>
2024-10-16 01:46:51 -0400

    test: Add another CreateUser API test to ensure user creation is atomic

--- tst/api/test_create_user.py
+++ tst/api/test_create_user.py
@@ -14,11 +14,19 @@ def get_create_user_event(email: str, username: str, password: str) -> dict:
 
 
 def test_create_user(walter_db) -> None:
-    event = get_create_user_event(
-        email="jim@gmail.com", username="jim", password="jim"
-    )
+    event = get_create_user_event(email="jim@gmail.com", username="jim", password="jim")
     expected_response = get_expected_response(
         status_code=HTTPStatus.OK, status=Status.SUCCESS, message="User created!"
+    )
+    assert expected_response == CreateUser(walter_db).invoke(event)
+
+
+def test_create_user_failure_user_already_exists(walter_db) -> None:
+    event = get_create_user_event(
+        email="walter@gmail.com", username="walter", password="walter"
+    )
+    expected_response = get_expected_response(
+        status_code=HTTPStatus.OK, status=Status.FAILURE, message="User already exists!"
     )
     assert expected_response == CreateUser(walter_db).invoke(event)
 
