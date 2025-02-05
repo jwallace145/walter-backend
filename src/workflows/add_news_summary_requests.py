1d1a41f3124175848632c724ad277083089b77d5
Jimmy Wallace <jimmy.wallace145@gmail.com>
2025-02-04 21:49:34 -0500

    feat: Add NewsSummaries queue for async news summary processing

---
+++ src/workflows/add_news_summary_requests.py
@@ -0,0 +1,35 @@
+import datetime as dt
+import json
+
+from src.clients import walter_db, news_summaries_queue
+from src.news.queue import NewsSummaryRequest
+from src.utils.log import Logger
+
+log = Logger(__name__).get_logger()
+
+
+def add_news_summary_requests_workflow(event, context) -> dict:
+    log.info("WalterWorkflow: Add News Summary Requests")
+
+    log.info(
+        "Scanning WalterDB Stocks table for all stocks to generate news summaries..."
+    )
+    for stock in walter_db.get_all_stocks():
+        log.info(
+            f"Add news summary request for stock '{stock.symbol.upper()}' to queue"
+        )
+        news_summaries_queue.add_news_summary_request(
+            NewsSummaryRequest(
+                datestamp=dt.datetime.now().replace(
+                    hour=0, minute=0, second=0, microsecond=0
+                ),
+                stock=stock.symbol.upper(),
+            )
+        )
+    log.info(
+        "Successfully scanned WalterDB Stocks table and submitted news summary requests for all stocks!"
+    )
+    return {
+        "statusCode": 200,
+        "body": json.dumps("WalterWorkflow: Add News Summary Requests"),
+    }
