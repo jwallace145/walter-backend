1cd538aa412e1c1fad546c67ab3b1686a4f5c9e2
Jimmy Wallace <jimmy.wallace145@gmail.com>
2025-02-12 01:17:58 +0100

    fix: Actually fix double logs (#185)

--- src/utils/log.py
+++ src/utils/log.py
@@ -1,11 +1,8 @@
+import logging
 import os
-import sys
 from dataclasses import dataclass
-from logging import FileHandler, Formatter, Handler, Logger, StreamHandler, getLogger
 
 import coloredlogs
-
-LOG_FILE = os.getenv("LOGS_WORKING_DIR", "./") + "logs.log"
 
 
 @dataclass
@@ -13,30 +10,14 @@ class Logger:
 
     name: str
     log_level: str = os.getenv("LOG_LEVEL", "INFO")
-    formatter: Formatter = Formatter(
-        "%(asctime)s :: %(levelname)s :: %(name)s :: %(message)s"
-    )
+    enable_colored_logs: bool = os.getenv("ENABLE_COLORED_LOGS", "False") == "True"
 
-    def _get_console_handler(self) -> Handler:
-        console_handler = StreamHandler(sys.stdout)
-        console_handler.setLevel(self.log_level)
-        console_handler.setFormatter(self.formatter)
-        return console_handler
-
-    def _get_file_handler(self, filename: str) -> Handler:
-        file_handler = FileHandler(filename, mode="a+")
-        file_handler.setLevel(self.log_level)
-        file_handler.setFormatter(self.formatter)
-        return file_handler
-
-    def get_logger(self) -> Logger:
-        logger = getLogger(self.name)
+    def get_logger(self) -> logging.Logger:
+        logger = logging.getLogger(self.name)
         logger.setLevel(self.log_level)
-        # logger.addHandler(self._get_console_handler())
-        # logger.addHandler(self._get_file_handler(filename=LOG_FILE))
-        coloredlogs.install(
-            fmt="%(asctime)s :: %(levelname)s :: %(name)s :: %(message)s",
-            level=self.log_level,
-            logger=logger,
-        )
+        if self.enable_colored_logs:
+            coloredlogs.install(
+                level=self.log_level,
+                logger=logger,
+            )
         return logger
