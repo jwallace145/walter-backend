2d8711bdb198c923e35fc890cc60d161afe73097
Jimmy Wallace <jimmy.wallace145@gmail.com>
2025-04-20 01:19:28 -0400

    feat: Use Python 3.12 ARM64 base image for WalterAPI (#290)

--- scripts/update-lambdas.sh
+++ scripts/update-lambdas.sh
@@ -1,11 +1,16 @@
+#!/bin/bash
+
+# Define the image URI as a variable
+IMAGE_URI="010526272437.dkr.ecr.us-east-1.amazonaws.com/walter/api:latest"
+
 echo Updating WalterAPI Auth source code with artifact from S3 \
 && aws lambda update-function-code --function-name WalterAPI-Auth-dev --s3-bucket walter-backend-src --s3-key walter-backend.zip --no-cli-pager \
 && echo Updating WalterAPI CreateUser source code with artifact from S3 \
 && aws lambda update-function-code --function-name WalterAPI-CreateUser-dev --s3-bucket walter-backend-src --s3-key walter-backend.zip --no-cli-pager \
 && echo Updating WalterAPI GetUser source code with artifact from S3 \
-&& aws lambda update-function-code --function-name WalterAPI-GetUser-dev --s3-bucket walter-backend-src --s3-key walter-backend.zip --no-cli-pager \
+&& aws lambda update-function-code --function-name WalterAPI-GetUser-dev --image-uri $IMAGE_URI --no-cli-pager \
 && echo Updating WalterAPI GetStock source code with artifact from S3 \
-&& aws lambda update-function-code --function-name WalterAPI-GetStock-dev --s3-bucket walter-backend-src --s3-key walter-backend.zip --no-cli-pager \
+&& aws lambda update-function-code --function-name WalterAPI-GetStock-dev --image-uri $IMAGE_URI --no-cli-pager \
 && echo Updating WalterAPI GetStatistics source code with artifact from S3 \
 && aws lambda update-function-code --function-name WalterAPI-GetStatistics-dev --s3-bucket walter-backend-src --s3-key walter-backend.zip --no-cli-pager \
 && echo Updating WalterAPI AddStock source code with artifact from S3 \
@@ -13,17 +18,17 @@ echo Updating WalterAPI Auth source code with artifact from S3 \
 && echo Updating WalterAPI DeleteStock source code with artifact from S3 \
 && aws lambda update-function-code --function-name WalterAPI-DeleteStock-dev --s3-bucket walter-backend-src --s3-key walter-backend.zip --no-cli-pager \
 && echo Updating WalterAPI GetPrices source code with artifact from S3 \
-&& aws lambda update-function-code --function-name WalterAPI-GetPrices-dev --s3-bucket walter-backend-src --s3-key walter-backend.zip --no-cli-pager \
+&& aws lambda update-function-code --function-name WalterAPI-GetPrices-dev --image-uri $IMAGE_URI --no-cli-pager \
 && echo Updating WalterAPI GetNewsSummary source code with artifact from S3 \
 && aws lambda update-function-code --function-name WalterAPI-GetNewsSummary-dev --s3-bucket walter-backend-src --s3-key walter-backend.zip --no-cli-pager \
 && echo Updating WalterAPI GetPortfolio source code with artifact from S3 \
-&& aws lambda update-function-code --function-name WalterAPI-GetPortfolio-dev --s3-bucket walter-backend-src --s3-key walter-backend.zip --no-cli-pager \
+&& aws lambda update-function-code --function-name WalterAPI-GetPortfolio-dev --image-uri $IMAGE_URI --no-cli-pager \
 && echo Updating WalterAPI SendNewsletter source code with artifact from S3 \
 && aws lambda update-function-code --function-name WalterAPI-SendNewsletter-dev --s3-bucket walter-backend-src --s3-key walter-backend.zip --no-cli-pager \
 && echo Updating WalterAPI GetNewsletter source code with artifact from S3 \
 && aws lambda update-function-code --function-name WalterAPI-GetNewsletter-dev --s3-bucket walter-backend-src --s3-key walter-backend.zip --no-cli-pager \
 && echo Updating WalterAPI GetNewsletters source code with artifact from S3 \
-&& aws lambda update-function-code --function-name WalterAPI-GetNewsletters-dev --s3-bucket walter-backend-src --s3-key walter-backend.zip --no-cli-pager \
+&& aws lambda update-function-code --function-name WalterAPI-GetNewsletters-dev --image-uri $IMAGE_URI --no-cli-pager \
 && echo Updating WalterAPI SendVerifyEmail source code with artifact from S3 \
 && aws lambda update-function-code --function-name WalterAPI-SendVerifyEmail-dev --s3-bucket walter-backend-src --s3-key walter-backend.zip --no-cli-pager \
 && echo Updating WalterAPI VerifyEmail source code with artifact from S3 \
@@ -51,11 +56,11 @@ echo Updating WalterAPI Auth source code with artifact from S3 \
 && echo Updating WalterWorkflow CreateNewsSummaryAndArchive source code with artifact from S3 \
 && aws lambda update-function-code --function-name WalterWorkflow-CreateNewsSummaryAndArchive-dev --s3-bucket walter-backend-src --s3-key walter-backend.zip --no-cli-pager \
 && echo Updating WalterAPI GetExpenses source code with artifact from S3 \
-&& aws lambda update-function-code --function-name WalterAPI-GetExpenses-dev --image-uri 010526272437.dkr.ecr.us-east-1.amazonaws.com/walter/api:latest --no-cli-pager \
+&& aws lambda update-function-code --function-name WalterAPI-GetExpenses-dev --image-uri $IMAGE_URI --no-cli-pager \
 && echo Updating WalterAPI AddExpense source code with artifact from S3 \
-&& aws lambda update-function-code --function-name WalterAPI-AddExpense-dev --image-uri 010526272437.dkr.ecr.us-east-1.amazonaws.com/walter/api:latest --no-cli-pager \
+&& aws lambda update-function-code --function-name WalterAPI-AddExpense-dev --image-uri $IMAGE_URI --no-cli-pager \
 && echo Updating WalterAPI DeleteExpense source code with artifact from S3 \
-&& aws lambda update-function-code --function-name WalterAPI-DeleteExpense-dev --image-uri 010526272437.dkr.ecr.us-east-1.amazonaws.com/walter/api:latest --no-cli-pager \
+&& aws lambda update-function-code --function-name WalterAPI-DeleteExpense-dev --image-uri $IMAGE_URI --no-cli-pager \
 && echo Updating WalterCanary AuthUser source code with artifact from S3 \
 && aws lambda update-function-code --function-name WalterCanary-AuthUserCanary-dev --s3-bucket walter-backend-src --s3-key walter-backend.zip --no-cli-pager \
 && echo Updating WalterCanary GetNewsletters source code with artifact from S3 \
