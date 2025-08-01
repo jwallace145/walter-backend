e68c35518f273c2083f2206f0ee5a5eec58f1331
Jimmy Wallace <jimmy.wallace145@gmail.com>
2025-07-31 23:58:55 -0600

    docs: add cli docs (#339)

---
+++ src/api/common/metrics.py
@@ -0,0 +1,17 @@
+###########
+# METRICS #
+###########
+
+# Metrics included in this file are emitted for all APIs
+
+METRICS_SUCCESS_COUNT = "SuccessCount"
+"""(str): The number of successful API invocations."""
+
+METRICS_FAILURE_COUNT = "FailureCount"
+"""(str): The number of failed API invocations."""
+
+METRICS_TOTAL_COUNT = "TotalCount"
+"""(str): The total number of API invocations."""
+
+METRICS_RESPONSE_TIME_MILLISECONDS = "ResponseTimeMilliseconds"
+"""(str): The response time for the API invocation in milliseconds."""
