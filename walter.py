0f82b0fd957acdf90cc710bcaf9fe46f86614b19
Jimmy Wallace <jimmy.wallace145@gmail.com>
2025-08-08 00:19:37 -0600

    fix: fix deployment bugs

--- walter.py
+++ walter.py
@@ -5,9 +5,8 @@ from src.canaries.routing.router import CanaryRouter, CanaryType
 def walter_api_entrypoint(event, context) -> dict:
     return APIRouter.get_method(event).invoke(event).to_json()
 
-  
+
 def walter_canaries_entrypoint(event, context) -> None:
     for canary in CanaryType:
         name = canary.value
         CanaryRouter.get_canary({"canary": name}).invoke()
-
