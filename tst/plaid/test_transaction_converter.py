d501878e842f716bd94a7022c97981a6cdca4ffe
Jimmy Wallace <jimmy.wallace145@gmail.com>
2025-09-23 23:24:07 -0600

    feat: add plaid transaction converter unit tests

--- tst/plaid/test_transaction_converter.py
+++ tst/plaid/test_transaction_converter.py
@@ -19,7 +19,9 @@ def transaction_converter(
     return TransactionConverter(walter_db, transactions_categorizer)
 
 
-def test_transaction_converter(transaction_converter: TransactionConverter) -> None:
+def test_transaction_converter_new_transaction(
+    transaction_converter: TransactionConverter,
+) -> None:
     plaid_account_id = "plaid-acct-001"
     plaid_transaction_id = "plaid-txn-001"
     account_id = "acct-001"
@@ -32,7 +34,56 @@ def test_transaction_converter(transaction_converter: TransactionConverter) -> N
     transaction = transaction_converter.convert(
         plaid_transaction, TransactionConversionType.NEW
     )
+    assert isinstance(transaction, BankTransaction)
+    assert transaction.plaid_account_id == plaid_account_id
+    assert transaction.plaid_transaction_id == plaid_transaction_id
+    assert transaction.account_id == account_id
+    assert transaction.merchant_name == merchant_name
+    assert transaction.transaction_amount == amount
+    assert transaction.get_transaction_date() == date
 
+
+def test_transaction_converter_updated_transaction(
+    transaction_converter: TransactionConverter,
+) -> None:
+    plaid_account_id = "plaid-acct-001"
+    plaid_transaction_id = "plaid-txn-001"
+    account_id = "acct-001"
+    merchant_name = "Uber"
+    amount = 10.00  # new amount
+    date = dt.datetime(2025, 8, 30)
+    plaid_transaction = create_plaid_transaction(
+        plaid_account_id, plaid_transaction_id, merchant_name, amount, date
+    )
+    transaction = transaction_converter.convert(
+        plaid_transaction, TransactionConversionType.UPDATED
+    )
+    # assert modified details for updated transaction
+    assert isinstance(transaction, BankTransaction)
+    assert transaction.plaid_account_id == plaid_account_id
+    assert transaction.plaid_transaction_id == plaid_transaction_id
+    assert transaction.account_id == account_id
+    assert transaction.merchant_name == merchant_name
+    assert transaction.transaction_amount == amount
+    assert transaction.get_transaction_date() == date
+
+
+def test_transaction_converter_deleted_transaction(
+    transaction_converter: TransactionConverter,
+) -> None:
+    plaid_account_id = "plaid-acct-001"
+    plaid_transaction_id = "plaid-txn-001"
+    account_id = "acct-001"
+    merchant_name = "Uber"
+    amount = 6.33
+    date = dt.datetime(2025, 8, 30)
+    plaid_transaction = create_plaid_transaction(
+        plaid_account_id, plaid_transaction_id, merchant_name, amount, date
+    )
+    transaction = transaction_converter.convert(
+        plaid_transaction, TransactionConversionType.NEW
+    )
+    # assert same details for a transaction to be deleted
     assert isinstance(transaction, BankTransaction)
     assert transaction.plaid_account_id == plaid_account_id
     assert transaction.plaid_transaction_id == plaid_transaction_id
