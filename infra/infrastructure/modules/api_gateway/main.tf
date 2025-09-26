fcf37d42efb701d7dc40655e2e5552226f794fb8
Jimmy Wallace <jimmy.wallace145@gmail.com>
2025-09-25 19:16:48 -0600

    fix: fix api gateway key

--- infra/infrastructure/modules/api_gateway/main.tf
+++ infra/infrastructure/modules/api_gateway/main.tf
@@ -13,7 +13,7 @@ resource "aws_api_gateway_usage_plan" "usage_plan" {
 
   api_stages {
     api_id = aws_api_gateway_rest_api.api.id
-    stage  = var.stage_name
+    stage  = aws_api_gateway_stage.api_stage.stage_name
   }
 
   throttle_settings {
